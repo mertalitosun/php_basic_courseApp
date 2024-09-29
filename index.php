3d62e48f60addb46156ccb4a93ba4d9ba376cf48
Mertali Tosun <mertali2635@icloud.com>
2024-09-29 19:08:53 +0300

    courseApp Başlangıç

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-lg bg-primary navbar dark">
+        <div class="container">
+            <a href="/" class="navbar-brand">Course App</a>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Anasayfa</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">Kurslar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container my-3">
+        <div class="row">
+            <div class="col-3">
+                <div class="list-group">
+                    <a href="" class="list-group-item list-group-item-action">Programlama</a>
+                    <a href="" class="list-group-item list-group-item-action">Web Geliştirme</a>
+                    <a href="" class="list-group-item list-group-item-action">Mobil Uygulama</a>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/1.jpg" alt="" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="cart-title">Web Geliştirme</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur hic odio, nostrum quas accusamus quod odit numquam illum iste dolor facere voluptatem repudiandae tenetur fuga, nisi sit quisquam a corporis!</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/2.jpg" alt="" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="cart-title">Python Kursu</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur hic odio, nostrum quas accusamus quod odit numquam illum iste dolor facere voluptatem repudiandae tenetur fuga, nisi sit quisquam a corporis!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="img/3.jpg" alt="" class="img-fluid rounded-start">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="cart-title">Javascript Kursu</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur hic odio, nostrum quas accusamus quod odit numquam illum iste dolor facere voluptatem repudiandae tenetur fuga, nisi sit quisquam a corporis!</p>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
